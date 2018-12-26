45e09f6ad5b2ae5211a53b348ed46372484197ee
Sheshnath Kumar <sheshnath@promoitech.com>
2018-12-26 15:47:10 +0530

    webRTC sample project added, this project is reciever of broadcast

---
+++ WebRTC-iOS-master 2/WebRTC/WebRTC/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  WebRTC
+//
+//  Created by Stas Seldin on 20/05/2018.
+//  Copyright © 2018 Stas Seldin. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
